df6a786b629579560a6f9edc4282f8dadd337c1e
takashiono2 <takashiono710@gmail.com>
2021-09-11 17:25:19 +0900

    seciton10の82

---
+++ section6/nuxt-async/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- {{ users[0].id }},{{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }},{{ user.name }},{{ user.company.name }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios')
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+  asyncData ({ params }) {
+    return axios.get(url).then((res) => {
+      return { users: res.data }
+    })
+  }
+}
+</script>
