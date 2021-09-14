49aec3c7c0864114fb231e529715673c9d2bc1ce
takashiono2 <takashiono710@gmail.com>
2021-09-14 12:52:08 +0900

    section4のファイル追加

---
+++ section4/nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>{{message}}</h1>
+    <p>id: {{ this.$route.params.id}}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return { message:'/user/_id.vueを表示中' }
+  },
+  validate({params}){
+    return /^\d+$/.test(params.id)//testの()の中身が数値の場合はtrue,数値でない場合はfalseを返す
+  }
+}
+
+</script>
+
+<style>
+
+</style>
